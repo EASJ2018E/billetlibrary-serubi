4b649f8d04fd3bc3f812c66cf0b6c198b389ba1a
serubi <serubi@live.com>
2018-09-17 12:38:57 +0200

    Tilføjet Bil klasse

---
+++ BilletLibrary/Bil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        // Instance fields
+        private string _nummerplade;
+        private DateTime _dato;
+
+        // Constructor
+        public Bil()
+        {
+
+        }
+
+        // Methods
+
+        /// <summary>
+        /// Get price for vehicle
+        /// </summary>
+        /// <returns></returns>
+        public decimal Pris()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// Get vehicle type name
+        /// </summary>
+        /// <returns></returns>
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+        // Backing fields
+        public string Nummberplade
+        {
+            get { return _nummerplade; }
+            set { _nummerplade = value; }
+        }
+
+        public DateTime Dato
+        {
+            get { return _dato; }
+            set { _dato = value; }
+        }
+    }
+}
